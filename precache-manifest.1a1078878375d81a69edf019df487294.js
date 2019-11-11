83597c6d402063f11c4712e03305b7fd812c5f56
Paul Hillmint <paulhillmint@Polaris-Chens-MacBook-Pro.local>
2019-11-10 20:58:47 -0800

    Updates

---
+++ precache-manifest.1a1078878375d81a69edf019df487294.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8bed1d0c860b5fd376158367eb47e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "657672eebe103aedeae1",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "18c5441d1b832a83214c",
+    "url": "/static/css/main.62ee3cbf.chunk.css"
+  },
+  {
+    "revision": "657672eebe103aedeae1",
+    "url": "/static/js/2.7257bab5.chunk.js"
+  },
+  {
+    "revision": "18c5441d1b832a83214c",
+    "url": "/static/js/main.40500beb.chunk.js"
+  },
+  {
+    "revision": "21519415f8c275e59b54",
+    "url": "/static/js/runtime-main.2cfd0138.js"
+  }
+]);
