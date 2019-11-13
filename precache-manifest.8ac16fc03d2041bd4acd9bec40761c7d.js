7a2f8d7d32809770f77820615aff4b7cdf91df2c
Paul Hillmint <paulhillmint@Polaris-Chens-MacBook-Pro.local>
2019-11-12 23:28:47 -0800

    Updates

---
+++ precache-manifest.8ac16fc03d2041bd4acd9bec40761c7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e82131145373a5f77ade3e15113d713c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01c1aece23ede491691d",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "f06678f2f8a608ee7106",
+    "url": "/static/css/main.d61f2ab0.chunk.css"
+  },
+  {
+    "revision": "01c1aece23ede491691d",
+    "url": "/static/js/2.a09aac1d.chunk.js"
+  },
+  {
+    "revision": "f06678f2f8a608ee7106",
+    "url": "/static/js/main.d0d2aa08.chunk.js"
+  },
+  {
+    "revision": "21519415f8c275e59b54",
+    "url": "/static/js/runtime-main.2cfd0138.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ea1cbc8f166cc1591af2dab0594f9bb0.js"
+  "/precache-manifest.8ac16fc03d2041bd4acd9bec40761c7d.js"
 );
 
 self.addEventListener('message', (event) => {
