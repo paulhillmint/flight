a03862fff5ad32d2205779f214cc7295f7b487f1
Paul Hillmint <paulhillmint@Polaris-Chens-MacBook-Pro.local>
2019-11-23 12:48:56 -0800

    Add list page

---
+++ src/components/ListPage/ListPage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container, Row, Col } from 'react-grid-system';
+
+import Section from '../Section';
+import InfoCard from '../InfoCard';
+
+import './ListPage.css';
+
+const ListPage = () => {
+  return (
+    <Container>
+      <Row gutterWidth={18}>
+        <Col md={8}>
+          <Section />
+          <Section />
+          <Section />
+        </Col>
+        <Col md={4}>
+          <InfoCard flightId='ABP-920' />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+
+export default ListPage;
