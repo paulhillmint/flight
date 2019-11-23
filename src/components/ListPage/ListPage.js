423d7160a24fe34f7b05c517813f71ba8136d59c
Paul Hillmint <paulhillmint@Polaris-Chens-MacBook-Pro.local>
2019-11-23 15:15:03 -0800

    Update list page

--- src/components/ListPage/ListPage.js
+++ src/components/ListPage/ListPage.js
@@ -2,21 +2,55 @@ import React from 'react';
 import { Container, Row, Col } from 'react-grid-system';
 
 import Section from '../Section';
+import Headline from '../Headline';
 import InfoCard from '../InfoCard';
 
 import './ListPage.css';
 
 const ListPage = () => {
+  const renderAirline = () => {
+    const list = [
+      {
+        series: 'SSNI',
+        startNumber: '658',
+        flightCount: 16
+      },
+      {
+        series: 'ABP',
+        startNumber: '933',
+        flightCount: 8
+      },
+      {
+        series: 'IPX',
+        startNumber: '421',
+        flightCount: 16
+      }
+    ];
+    return list.map(({ series, startNumber, flightCount }) => (
+      <Section series={series} startNumber={startNumber} flightCount={flightCount} key={series} />
+    ));
+  };
+
+  const renderToday = () => {
+    const list = [ 'SSNI-643', 'IPX-415', 'SSNI-658' ];
+    return list.map(flightId => (
+      <Col lg={12} key={flightId}>
+        <InfoCard flightId={flightId} isMini={true} isFull={true} />
+      </Col>
+    ));
+  };
+
   return (
     <Container>
       <Row gutterWidth={18}>
         <Col md={8}>
-          <Section series='SSNI' startNumber='658' flightCount={16} />
-          <Section series='ABP' startNumber='933' flightCount={8} />
-          <Section series='IPX' startNumber='421' flightCount={16} />
+          {renderAirline()}
         </Col>
         <Col md={4}>
-          <InfoCard flightId='ABP-920' />
+          <Headline title={`Today's Special`} />
+          <Row gutterWidth={18}>
+            {renderToday()}
+          </Row>
         </Col>
       </Row>
     </Container>
