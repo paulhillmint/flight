c2a61fc9191e08ecbcbad1950be25cd6693a9f2c
Paul Hillmint <paulhillmint@Polaris-Chens-MacBook-Pro.local>
2019-11-23 22:54:20 -0800

    Update list page

--- src/components/ListPage/ListPage.js
+++ src/components/ListPage/ListPage.js
@@ -1,59 +1,71 @@
 import React from 'react';
+import { useHistory, useParams } from 'react-router-dom';
 import { Container, Row, Col } from 'react-grid-system';
+import { IoIosAirplane } from 'react-icons/io';
+import { MdLabel, MdExposurePlus1 } from 'react-icons/md';
+
+import { urlFinder, airlineURLFormatter } from '../../utils/urlUtil';
 
 import Section from '../Section';
 import Headline from '../Headline';
 import InfoCard from '../InfoCard';
+import Card from '../Card';
+import WebLink from '../WebLink';
 
 import './ListPage.css';
 
-const ListPage = () => {
-  const renderAirline = () => {
-    const list = [
-      {
-        series: 'SSNI',
-        startNumber: '658',
-        flightCount: 16
-      },
-      {
-        series: 'ABP',
-        startNumber: '933',
-        flightCount: 8
-      },
-      {
-        series: 'IPX',
-        startNumber: '421',
-        flightCount: 16
-      }
-    ];
-    return list.map(({ series, startNumber, flightCount }) => (
-      <Section series={series} startNumber={startNumber} flightCount={flightCount} key={series} />
-    ));
-  };
+const airlineList = [ 'SSNI', 'ABP', 'IPX' ];
 
-  const renderToday = () => {
-    const list = [ 'SSNI-643', 'IPX-415', 'SSNI-658' ];
-    return list.map(flightId => (
-      <Col lg={12} key={flightId}>
-        <InfoCard flightId={flightId} isMini={true} isFull={true} />
-      </Col>
-    ));
-  };
+const ListPage = () => {
+  let { airlineId } = useParams();
+  airlineId = airlineId.toUpperCase();
+  if (airlineList.includes(airlineId)) {
+    // Airline page
+  } else {
+    // Index page
+  }
 
   return (
     <Container>
       <Row gutterWidth={18}>
         <Col md={8}>
-          {renderAirline()}
+          <Section series={airlineId} startNumber={658} flightCount={120} />
         </Col>
         <Col md={4}>
-          <Headline title={`Today's Special`} />
+          <Headline title={`All Airlines`} />
           <Row gutterWidth={18}>
-            {renderToday()}
+            <Col lg={12}>
+              <AirlineListCard title='Legend Airlines' list={[ 'SSNI', 'IPX', 'ABP' ]} />
+              <AirlineListCard title='Fashion Airlines' list={[ 'STARS', 'PPPD', 'WANZ', 'MIDE' ]} />
+              <AirlineListCard title='Classic Airlines' list={[ 'SNIS', 'SOE', 'IPZ', 'IPTD', 'ABS' ]} />
+            </Col>
           </Row>
         </Col>
       </Row>
     </Container>
+  );
+};
+
+const AirlineListCard = props => {
+  const { title, list } = props;
+
+  const renderAirlines = () => {
+    return list.map(airlineId => (
+      <WebLink url={airlineURLFormatter(airlineId)} title={`Airline ${airlineId}`} icon={<MdLabel />} />
+    ));
+  };
+
+  return (
+    <Card
+      header={
+        <h2>{title}</h2>
+      }
+      footer={
+        renderAirlines()
+      }
+    >
+      <div className='Card-line'></div>
+    </Card>
   );
 };
 
