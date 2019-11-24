7b508be7280c52f48f09fde78277eeb7b631aa65
Paul Hillmint <paulhillmint@Polaris-Chens-MacBook-Pro.local>
2019-11-23 23:16:54 -0800

    Add nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-grid-system';
+import { FaRegStar } from 'react-icons/fa';
 
 import './Header.css';
 
@@ -10,13 +11,22 @@ const Header = () => {
         <Row>
           <Col sm={4}>
             <div className='Header-title code'>
-              <a className="ga-index" href='/' target='_blank' rel='noopener noreferrer'>
+              <a className="ga-nav" id="index" href='/' target='_blank' rel='noopener noreferrer'>
                 ✈Flight
               </a>
             </div>
           </Col>
           <Col sm={8}>
-            <div className='Header-nav'>
+            <div className='Header-nav code'>
+              <a className="ga-nav" id="flights" href='/' target='_blank' rel='noopener noreferrer'>
+                Hall
+              </a>
+              <a className="ga-nav" id="flights" href='/about' target='_blank' rel='noopener noreferrer'>
+                About
+              </a>
+              <a className="ga-nav" id="flights" href='/' target='_blank' rel='noopener noreferrer'>
+                <FaRegStar />
+              </a>
             </div>
           </Col>
         </Row>
